82322b4753098d381ce5f56ecbc0f9d8838bb409
Marie Leleu <marieleleu@icloud.com>
2020-11-13 12:55:08 +0100

    cocktail app fully funtioning

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Cocktail.destroy_all
+Ingredient.destroy_all
+Dose.destroy_all
+
+lemon = Ingredient.create(name: 'lemon')
+ice = Ingredient.create(name: 'ice')
+mint_leaves = Ingredient.create(name: 'mint leaves')
+tequila = Ingredient.create(name: 'tequila')
+gin = Ingredient.create(name: 'gin')
+lime = Ingredient.create(name: 'lime')
+vodka = Ingredient.create(name: 'vodka')
+tonic = Ingredient.create(name: 'tonic')
+orange_liqueur = Ingredient.create(name: 'orange liqueur')
+sugar = Ingredient.create(name: 'sugar')
+
+mojito = Cocktail.create(name: 'mojito')
+
+margarita = Cocktail.create(name: 'margarita')
+
+gin_tonic = Cocktail.create(name: 'gin tonic')
